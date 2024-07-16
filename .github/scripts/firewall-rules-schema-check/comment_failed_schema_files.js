d7162a4121450570fab4654ac29b89785e2f328a
Robert Teller <robert.j.c.teller@gmail.com>
2024-07-16 15:40:26 -0700

    updating variables

--- .github/scripts/firewall-rules-schema-check/comment_failed_schema_files.js
+++ .github/scripts/firewall-rules-schema-check/comment_failed_schema_files.js
@@ -1,15 +1,19 @@
-module.exports = async ({ github, context, core }) => {
-  const jsonSchemaValidationErrors = JSON.parse(core.getInput("jsonSchemaValidationErrors"));
-
+module.exports = async ({
+  github,
+  context,
+  core,
+  jsonSchemaValidationErrors,
+}) => {
+  //   const jsonSchemaValidationErrors = JSON.parse(core.getInput("jsonSchemaValidationErrors"));
 
   let commentBody = "### 🔴 Schema Validation Failed\n\n";
   commentBody += "The following files failed schema validation:\n\n";
 
   if (jsonSchemaValidationErrors.length > 0) {
     commentBody += "#### Schema Validation Errors:\n";
-    jsonSchemaValidationErrors.forEach(error => {
+    jsonSchemaValidationErrors.forEach((error) => {
       commentBody += `- **File**: ${error.filename}\n`;
-      error.errors.forEach(err => {
+      error.errors.forEach((err) => {
         commentBody += `  - **Error**: ${err.message}\n`;
       });
     });
@@ -19,6 +23,6 @@ module.exports = async ({ github, context, core }) => {
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: context.issue.number,
-    body: commentBody
+    body: commentBody,
   });
 };
