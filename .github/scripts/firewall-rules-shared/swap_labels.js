51e49e0410772514bc63c3cbbbb44e0c0bf4f5bc
Robert Teller <robert.j.c.teller@gmail.com>
2024-07-16 15:27:27 -0700

    removing function call

--- .github/scripts/firewall-rules-shared/swap_labels.js
+++ .github/scripts/firewall-rules-shared/swap_labels.js
@@ -8,37 +8,41 @@
  * @param {string} add_label_name - The label to be added.
  * @param {string} [remove_label_name] - The label to be removed (optional).
  */
-module.exports = async function swapLabels(github, context, add_label_name, remove_label_name = null) {
-    const owner = context.repo.owner;
-    const repo = context.repo.repo;
-    const issue_number = context.issue.number;
-  
-    // Get the list of labels assigned to the pull request
-    const { data: labels } = await github.rest.issues.listLabelsOnIssue({
+module.exports = async (
+  github,
+  context,
+  add_label_name,
+  remove_label_name = null
+) => {
+  const owner = context.repo.owner;
+  const repo = context.repo.repo;
+  const issue_number = context.issue.number;
+
+  // Get the list of labels assigned to the pull request
+  const { data: labels } = await github.rest.issues.listLabelsOnIssue({
+    owner,
+    repo,
+    issue_number,
+  });
+
+  const labelNames = labels.map((label) => label.name);
+
+  // Check if remove_label_name is provided and assigned
+  if (remove_label_name && labelNames.includes(remove_label_name)) {
+    // Remove the specified label
+    await github.rest.issues.removeLabel({
       owner,
       repo,
       issue_number,
-    });
-  
-    const labelNames = labels.map((label) => label.name);
-  
-    // Check if remove_label_name is provided and assigned
-    if (remove_label_name && labelNames.includes(remove_label_name)) {
-      // Remove the specified label
-      await github.rest.issues.removeLabel({
-        owner,
-        repo,
-        issue_number,
-        name: remove_label_name,
-      });
-    }
-  
-    // Add the new label
-    await github.rest.issues.addLabels({
-      owner,
-      repo,
-      issue_number,
-      labels: [add_label_name],
+      name: remove_label_name,
     });
   }
-  
+
+  // Add the new label
+  await github.rest.issues.addLabels({
+    owner,
+    repo,
+    issue_number,
+    labels: [add_label_name],
+  });
+};
