2de9aedc0e9d0b2d4763bb528858eed8d71fb3a4
Robert Teller <robert.j.c.teller@gmail.com>
2024-07-16 07:26:24 -0700

    update scripts

--- .github/scripts/terraform-schema-check/get_list_changed_files.js
+++ .github/scripts/terraform-schema-check/get_list_changed_files.js
@@ -1,29 +1,36 @@
-// Define the list of path prefixes you want to filter by
-const pathPrefixes = [
-  // Add more prefixes as needed
-  "rules/gcp-workflow-firewall-rules/",
-];
+module.exports = async ({ github, context, core }) => {
+  // Define the list of path prefixes you want to filter by
+  const pathPrefixes = [
+    // Add more prefixes as needed
+    "rules/gcp-workflow-firewall-rules/",
+  ];
 
-// Define the list of file extensions you want to include
-const allowedExtensions = [
-  // Add more extensions as needed
-  ".json",
-];
+  // Define the list of file extensions you want to include
+  const allowedExtensions = [
+    // Add more extensions as needed
+    ".json",
+  ];
 
-const payload = context.payload.pull_request;
+  const payload = context.payload.pull_request;
 
-const { data: diff } = await github.rest.pulls.listFiles({
-  owner: context.repo.owner,
-  repo: context.repo.repo,
-  pull_number: payload.number,
-});
+  try {
+    const { data: diff } = await github.rest.pulls.listFiles({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: payload.number,
+    });
 
-const changedFiles = diff
-  .map((file) => file.filename)
-  .filter(
-    (file) =>
-      allowedExtensions.includes(file.endsWith(".json")) &&
-      pathPrefixes.some((prefix) => file.startsWith(prefix))
-  );
-console.log(changedFiles);
-core.setOutput("changedFiles", changedFiles);
+    const changedFiles = diff
+      .map((file) => file.filename)
+      .filter(
+        (file) =>
+          allowedExtensions.some((ext) => file.endsWith(ext)) &&
+          pathPrefixes.some((prefix) => file.startsWith(prefix))
+      );
+    console.log(changedFiles);
+    core.setOutput("changedFiles", changedFiles);
+    return changedFiles;
+  } catch (error) {
+    core.setFailed(`Error getting changed files: ${error.message}`);
+  }
+};
